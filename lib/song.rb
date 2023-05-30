37eca536dd4f55cc29858e3213a1e4ddde1cb9a3
KiruiTito <titokirui402@gmail.com>
2023-05-30 09:22:41 +0300

    Completed

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,17 @@
 class Song
+  # ...
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+    SQL
 
-  attr_accessor :name, :album
-
-  def initialize(name:, album:)
-    @name = name
-    @album = album
+    DB[:conn].execute(sql)
   end
 
+  # ...
 end
